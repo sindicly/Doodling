71470af59a201bb3c15e08fcaa20b399b91805de
sindicly <sindi.chen@coassets.com.cn>
2019-01-14 14:53:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/it_android/doodling/WriteDilogListener.java
@@ -0,0 +1,16 @@
+package com.example.it_android.doodling;
+/**
+
+ * 监听手写板对话框
+
+ * @author chengcj1
+
+ *
+
+ */
+
+public interface WriteDilogListener {
+
+    public void onPaintDone(Object object);
+
+}
